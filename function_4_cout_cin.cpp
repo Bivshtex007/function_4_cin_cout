979b88cb10f6716bf47846020750893f34cea60f
Bivshtex007 <103932684+Bivshtex007@users.noreply.github.com>
2022-08-05 15:26:12 +0300

    Add files via upload

---
+++ function_4_cout_cin.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void hello() {
+
+	cout << "Hello ";
+
+}
+
+void world() {
+
+	cout << " World" << endl;
+
+}
+
+int main() {
+
+	hello();
+	world();
+	return 0;
+
+}
